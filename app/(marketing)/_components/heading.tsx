2e6f897122aa79f15b87d749e2e7900d5070b0ef
darshp623 <darshp717@gmail.com>
2024-08-07 21:36:27 -0400

    fixed centering

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -7,11 +7,11 @@ export const Heading = () => {
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
-        Your ideas, Documents, &Plans. Unified. Welcome to {" "}
-        <span className="underline">Note-Us</span>
+        Your Ideas, Documents, & Plans. Unified. Welcome to {" "}
+        <span className="underline">NoteUs</span>
       </h1>
       <h3 className="text-base sm:text-xl md:text-2xl font-medium">
-        Note-Us is a connected WorkSpace where <br />
+        NoteUs is a connected workspace where <br />
         better, faster, work happens.
       </h3>
       <Button>
